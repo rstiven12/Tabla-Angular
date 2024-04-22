ecbf7d102114162ec3f96255a4cb628443a31760
YEINN STEVIN BERRIO HOLGUIN <yberrioh@Grupo-Exito.com>
2024-04-22 16:00:31 -0500

    first commit

---
+++ src/app/data/people.data.ts
@@ -0,0 +1,144 @@
+import { Person } from '../models';
+
+export const People: Person[] = [
+  {
+    id: '1',
+    name: 'Alan Bello',
+    category: 'manager',
+    company: 'h&m',
+    levelOfHappiness: 'testing'
+  },
+  {
+    id: '2',
+    name: 'Alejandro Danza',
+    category: 'manager',
+    company: 'Dollar city',
+    levelOfHappiness: 'Ing.software'
+  },
+  {
+    id: '3',
+    name: 'Sara Grecia',
+    category: 'manager',
+    company: 'Carulla',
+    levelOfHappiness: 'ing.sistema'
+  },
+  {
+    id: '4',
+    name: 'Ramona Llanura',
+    category: 'maestra',
+    company: 'UNAC',
+    levelOfHappiness: 'Licenciada en ingles'
+  },
+  {
+    id: '5',
+    name: 'Alejandro Pedros',
+    category: 'manager',
+    company: 'Pablon tobon',
+    levelOfHappiness: 'testing'
+  },
+  {
+    id: '6',
+    name: 'Pablo Ducal',
+    category: 'manager',
+    company: 'Wokrking',
+    levelOfHappiness: 'desarrollador'
+  },
+  {
+    id: '7',
+    name: 'Samuel Glorias',
+    category: 'manager',
+    company: 'M19',
+    levelOfHappiness: 'Diseñador '
+  },
+  {
+    id: '8',
+    name: 'andres',
+    category: 'manager',
+    company: 'viajes exito',
+    levelOfHappiness: 'Documentador'
+  },
+  {
+    id: '9',
+    name: 'jarol',
+    category: 'empleado',
+    company: 'Cocina s.a.s',
+    levelOfHappiness: 'cocinero'
+  },
+  {
+    id: '10',
+    name: 'lucia',
+    category: 'manager',
+    company: 'kfc',
+    levelOfHappiness: 'manager project'
+  },
+  {
+    id: '11',
+    name: 'carlos yepes',
+    category: 'empleado',
+    company: 'aseos s.a.s',
+    levelOfHappiness: 'aseadora'
+  },
+  {
+    id: '12',
+    name: 'luis diaz',
+    category: 'empleado',
+    company: 'uber',
+    levelOfHappiness: 'conductor'
+  },
+  {
+    id: '13',
+    name: 'Nicolas',
+    category: 'empleado',
+    company: 'Rappi',
+    levelOfHappiness: 'conductor'
+  },
+  {
+    id: '14',
+    name: 'jorge',
+    category: 'jefe',
+    company: 'exito',
+    levelOfHappiness: 'desarrollo e innovación'
+  },
+  {
+    id: '15',
+    name: 'Joann zuasa',
+    category: 'empleado',
+    company: 'yzt',
+    levelOfHappiness: 'desarrollador'
+  },
+  {
+    id: '16',
+    name: 'Evelyn lozano',
+    category: 'empleada',
+    company: 'tigo',
+    levelOfHappiness: 'call center'
+  },
+  {
+    id: '17',
+    name: 'michael',
+    category: 'jefa',
+    company: 'carulla',
+    levelOfHappiness: 'ingeniera'
+  },
+  {
+    id: '18',
+    name: 'Isac Cope',
+    category: 'jefe',
+    company: 'avianca',
+    levelOfHappiness: 'ing'
+  },
+  {
+    id: '19',
+    name: 'Samara Emery',
+    category: 'empleada',
+    company: 'zara',
+    levelOfHappiness: 'secretaria'
+  },
+  {
+    id: '20',
+    name: 'Elisa Hammond',
+    category: 'empleada',
+    company: 'd1',
+    levelOfHappiness: 'desarrolladora'
+  }
+];
